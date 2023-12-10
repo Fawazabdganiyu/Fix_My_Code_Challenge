701f0bca26742af0eb2ef3de66e48364d8b0339e
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-10 17:08:02 +0100

    Include documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""
+Square class
+"""
+
 
 class square():
+    """ Square class definition """
 
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
+        """ Instantiation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +20,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ str representation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
